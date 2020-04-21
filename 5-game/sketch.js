dab6f48dbaf4d94a259dd58a228e0959175dba78
shawjulia1 <shawjulia1@gmail.com>
2020-04-21 11:55:11 -0700

    created a key

---
+++ 5-game/sketch.js
@@ -0,0 +1,21 @@
+function setup(){
+  createCanvas(600, 400);
+}
+
+function draw(){
+  background("white")
+}
+
+function key(){
+  textsize(32);
+  fill("black");
+  text('FIND:',10,30);
+  text('apple',10,60);
+  text('banana',10,90);
+  text('snowman',10,120);
+  text('horseshoe',10,150);
+  text('fish',10,180);
+  text('teacup',10,210);
+  text('cherries',10,240)
+
+}
